118d93849972f40138ff20994255978c4cdb1112
baljadrawy <157516849+baljadrawy@users.noreply.github.com>
2025-12-14 22:43:26 +0300

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,10 @@
 ```javascript
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+}
+
 module.exports = nextConfig
 ```
 
